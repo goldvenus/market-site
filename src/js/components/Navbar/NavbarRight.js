51d1e3278bda1d38e17788d3a54cde79a2e92081
Dhaval Zala <dhaval.zala@live.com>
2018-12-10 18:59:59 +0530

    Added header user menu

--- src/js/components/Navbar/NavbarRight.js
+++ src/js/components/Navbar/NavbarRight.js
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const NavbarRight = () => (
-  <ul className="theme-nav-right">
-    <li>
-      <Link to="/ListGear" >List Gear</Link>
-    </li>
-    <li>
-      <Link to="/RentGear" >Rent Gear</Link>
-    </li>
-    <li>
-      <button className="theme-btn theme-btn-outline-white ml-3 theme-btn-link">
-        <Link to="/login">Login</Link>
-      </button>
-    </li>
-    <li>
-      <button className="theme-btn theme-btn-filled-white ml-2 theme-btn-link">
-        <Link to="/register">Register</Link>
-      </button>
-    </li>
-  </ul>
-);
+class NavbarRight extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render() {
+    return (
+      <ul className="theme-nav-right">
+        <li>
+          <Link to="/ListGear" >List Gear</Link>
+        </li>
+        <li>
+          <Link to="/RentGear" >Rent Gear</Link>
+        </li>
+        <li>
+          <button className="theme-btn theme-btn-outline-white ml-3 theme-btn-link">
+            <Link to="/login">Login</Link>
+          </button>
+        </li>
+        <li>
+          <button className="theme-btn theme-btn-outline-white ml-3 theme-btn-link">
+            <Link to="/register">Register</Link>
+          </button>
+        </li>
+        <li>
+          <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle caret>
+              <div className="flex-row">
+                <img src="/static/media/profile-pic.41adcd33.jpg"/>
+                <div>Hello Markus</div>
+              </div>
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem><Link to="/dashboard">My Account</Link></DropdownItem>
+              <DropdownItem>Logout</DropdownItem>
+            </DropdownMenu>
+          </ButtonDropdown>
+        </li>
+        <li>
+        </li>
+      </ul>
+    );
+  }
+}
 
 export default NavbarRight;
