b1f465f6310556b587cae47304eaca3dc61e8fff
Golden Venus <jostar0610@gmail.com>
2019-03-05 14:03:06 +0100

    feat(Stories): animation

--- src/js/utils/animation.js
+++ src/js/utils/animation.js
@@ -1,5 +1,5 @@
-export const ANIMAITON_DURATION_1 = 100;
-export const ANIMAITON_DURATION_2 = 80;
+export const ANIMAITON_DURATION_1 = 200;
+export const ANIMAITON_DURATION_2 = 100;
 export const ANIMATION_DONE_EVENT = 'animation::done';
 
 export const triggerAnimationDoneEvent = node =>
