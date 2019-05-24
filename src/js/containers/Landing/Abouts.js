6404920ae72e5e22b840a1fc61fa162d78de3c66
Golden Venus <jostar0610@gmail.com>
2019-05-24 13:23:21 +0300

    aboutUs page rebuild

--- src/js/containers/Landing/Abouts.js
+++ src/js/containers/Landing/Abouts.js
@@ -1,58 +1,49 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-// import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import {Link} from "react-router-dom";
+
+const team = [
+  { name: 'Joe Stapleton', job_title: 'Co-founder', img: '/images/team/1.jpg' },
+  { name: 'Jakob Storm', job_title: 'Co-founder', img: '/images/team/2.jpg' },
+  { name: 'Josh Kocaurek', job_title: 'CEO', img: '/images/team/3.jpg' },
+  { name: 'Tomass Sola', job_title: 'Lead Sales Manager', img: '/images/team/5.jpg' },
+  { name: 'Joe Stapleton', job_title: 'Co-founder', img: '/images/team/1.jpg' },
+  { name: 'Jakob Storm', job_title: 'Co-founder', img: '/images/team/2.jpg' },
+  { name: 'Josh Kocaurek', job_title: 'CEO', img: '/images/team/3.jpg' },
+  { name: 'Tomass Sola', job_title: 'Lead Sales Manager', img: '/images/team/5.jpg' },
+];
 
 class About extends Component {
   
   render() {
     return (
       <div className="about-us">
-        <div className="about-us-head">
-          <div className="heading-mobile">
-              <Breadcrumb className="theme-text-small">
-                  <BreadcrumbItem>Home </BreadcrumbItem>
-                  <BreadcrumbItem active>About Us</BreadcrumbItem>
-              </Breadcrumb>
-              <h1>About Us</h1>
-          </div>
-          <div className="head-container">
-            <div className="container">
-              <div className="head-left">
-                <Breadcrumb className="theme-text-small">
-                  <BreadcrumbItem>Home </BreadcrumbItem>
-                  <BreadcrumbItem active>About Us</BreadcrumbItem>
-                </Breadcrumb>
-              </div>
-            </div>
-          </div>
-        </div>
-        
         <div className="about-us-body">
           <Container>
             <Row className='home-subsection'>
               <Col lg='12' md='24' className='order-lg-1 order-md-2'>
-                <h2>Rent the Gear<br/>Your Ideas Need</h2>
-                <p>If you love creating, you are most likely familiar with the problem of feeling limited by your gear. With Creative Market, you can now rent any gear you might need from other creatives around you.</p>
-                <Link to='/rentgear'><button className='theme-btn theme-btn-primary'>RENT GEAR</button></Link>
+                <h2>About Us</h2>
+                <p>Founded in 2018, Creative Market is a peer-to-peer rental marketplace allowing you to find and rent gear from other creatives around you. Cameras, Musical Instruments, Lights and anything else that comes to mind, Creative Market is the place to find it.</p>
+                <p>Creative Market is a part of Ketchup Creative ehf., a privately held company registered in Iceland.</p>
+                <Link to='/contact'><button className='theme-btn theme-btn-primary contact-btn'>CONTACT</button></Link>
+                <Link to='/termsofuse'><button className='theme-btn theme-btn-filled-white terms-btn'>TERMS OF USE</button></Link>
               </Col>
-              <Col lg='12' md='24' className='rent-gear-back-img back-img order-lg-2 order-md-1'/>
+              <Col lg='12' md='24' className='order-lg-2 order-md-1'>
+                <Row className='user-avatar-container'>
+                  {
+                    team.map((item, key) =>
+                      <div className='user-avatar' key={key} style={{'backgroundImage': `url(${item.img})`}}/>)
+                  }
+                </Row>
+              </Col>
             </Row>
             <Row className='home-subsection'>
-              <Col lg='12' md='24' className='add-gear-back-img back-img'/>
+              <Col lg='12' md='24' className='question-back-img back-img'/>
               <Col lg='12' md='24'>
-                <h2>List Your Own<br/>Gear for Rent</h2>
-                <p>When you’re not renting from others, allow others to rent from you. Making your fancy gear work for you, even when you’re not using it yourself. It’s a win-win situation!</p>
-                <Link to='/addgear'><button className='theme-btn theme-btn-primary'>ADD GEAR</button></Link>
+                <h2>Got Questions?</h2>
+                <p>If you have any questions, head on over to our FAQ page and you should find the answers before you even ask! Security, payments, insurance and more imporant topics. If the answer isn’t there, submit a new question through the form and we’ll get back to you as soon as possible.</p>
+                <Link to='/FAQ'><button className='theme-btn theme-btn-primary'>FAQs</button></Link>
               </Col>
-            </Row>
-            <Row className='home-subsection'>
-              <Col lg='12' md='24' className='order-lg-1 order-md-2'>
-                <h2>Secure Worldwide<br/>Rental Community</h2>
-                <p>Every member of Creative Market must be verified through our platform before engaging in rentals. Renters must complete payments before gear pickup takes place, and funds are securely held in escrow until owner and renter have both confirmed that the handoff has taken place.</p>
-                <Link to='/login'><button className='theme-btn theme-btn-primary'>SIGN UP</button></Link>
-              </Col>
-              <Col lg='12' md='24' className='secure-back-img back-img order-lg-2 order-md-1'/>
             </Row>
           </Container>
         </div>
