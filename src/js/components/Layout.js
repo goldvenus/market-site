cc1cd42172cc009f91164498947fac0bab762f1d
Golden Venus <jostar0610@gmail.com>
2019-06-22 02:44:15 +0300

    cookie popup update

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -102,8 +102,8 @@ class Layout extends Component {
         <CookieConsent
           location="bottom"
           buttonText="I agree"
-          style={{ background: "#d2003d", textAlign: "center" }}
-          buttonStyle={{ background: "white", color: "#f82462", fontSize: "15px", fontWeight: "600", padding: "5px 15px" }}
+          style={{ background: "white", textAlign: "center", color: "#f82462", "boxShadow": "rgba(173, 173, 173, 0.2) 0px 0px 26px 4px" }}
+          buttonStyle={{ background: "#f82462", color: "white", fontSize: "15px", padding: "5px 15px" }}
           onAccept={() => {}}
         >
           Before using this site you must agree to the <span style={{textDecoration: 'underline'}}><Link to='/privacy'>privacy and cookie policy</Link></span>.
